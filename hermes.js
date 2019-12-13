8536325079495126a413f8c419269087b54ab552
josarthas <josafato007@hotmail.com>
2019-12-13 13:12:40 -0600

    x

--- hermes.js
+++ hermes.js
@@ -1,7 +1,7 @@
 const mariadb = require('mariadb');
 const pool = mariadb.createPool({
      host: 'localhost',
-     user:'josarthas',
+     user:'root',
      password: 'ID11135PrisciLiz',
      connectionLimit: 5
 });
@@ -9,6 +9,8 @@ async function asyncFunction() {
   let conn;
   try {
 	conn = await pool.getConnection();
+  const res = await conn.query("USE hermes");
+  console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
 	const rows = await conn.query("SELECT 1 as val");
 	console.log(rows); //[ {val: 1}, meta: ... ]
 	const res = await conn.query("INSERT INTO usuarios VALUES ( 'prueba','prueba','prueba2','user' )", [1, "mariadb"]);
