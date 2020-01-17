ea69951d0326a7cf891128f1ac35f9156da38501
josarthas <josafato007@hotmail.com>
2020-01-17 00:18:31 -0600

    x

--- hermes.js
+++ hermes.js
@@ -14,6 +14,18 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, "public"))); //se define directorio externo, puede usarse para cualquier otro
 app.set('view engine', 'pug');
+
+
+
+var mariaconn = mariadb.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  connectionLimit: process.env.DB_CONNLMT,
+  port: process.env.DB_PORT,
+  database: process.env.DB_DB
+});
+
 const pool = mariadb.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -23,38 +35,15 @@ const pool = mariadb.createPool({
   database: process.env.DB_DB
 }); //no requiere seguridad extra al correr directamente sobre el servidor local.
 console.log(pool);
-async function asyncFunction() {
-  let conn;
-  console.log(conn);
-  try {
-    conn = await pool.getConnection();
-    const res = await conn.query("INSERT INTO usuarios VALUES ( 'pruebauser','753951','prueba2','user' )", [1, "mariadb"]);
-    console.log(res);
-  } catch (err) {
-    throw err;
-    console.log(err);
-  } finally {
-    if (conn) return conn.end();
-  }
-} //entran datos una y otra vez a base de datos
+
 app.get('/', function(soli, resp) {
   resp.render('login');
 });
+
 app.post('/login', function(soli, resp) {
   var data = {
     usuario: soli.body.usuario,
     password: soli.body.password
-  }
-  console.log(data);
-  let conn;
-  try {
-    conn = pool.getConnection();
-    const resp = conn.query("SELECT FROM usuarios ", data, [1, "mariadb"]);
-  } catch (err) {
-    throw (err);
-    console.log(err);
-  } finally {
-    if (conn) return conn.end();
   }
   resp.render('layout');
 });
@@ -63,3 +52,17 @@ app.post('/login', function(soli, resp) {
   console.log('Listening on port ' + listener.address().port);
 }, null, true, 'America/Los_Angeles');
 */
+
+
+
+mariaconn.connect(function(err)) {
+    mariaconn => {
+      mariaconn.query("SELECT * FROM usuarios WHERE usuario = ", +Park Lane 38, function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    })
+  .catch(err => {
+    //handle connection error
+  });
+}
